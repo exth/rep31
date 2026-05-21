a24be4a482d4bfcfc34286b09eb0e62bf3fc5f51
Вадим <exth331@gmail.com>
2026-05-21 04:42:41 +0300

    commit 1

---
+++ test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Вадим on 21.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
